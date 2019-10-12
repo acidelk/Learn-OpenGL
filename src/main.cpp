1f94dc4c9b73c9f32962abe4b2ad9568c0dc89e3
Anton Fedosov <fed.guman@gmail.com>
2019-10-12 13:23:38 +0300

    support conan dependency manager

--- src/main.cpp
+++ src/main.cpp
@@ -5,50 +5,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
+#include "shader.h"
 
-const GLchar *vertex_shader_source = R"glsl(
-    #version 330 core
-    layout (location = 0) in vec3 position;
-    void main()
-    {
-        gl_Position = vec4(position, 1.0);
-    }
-)glsl";
-
-const GLchar *fragment_shader_source = R"glsl(
-    #version 330 core
-    out vec4 color;
-    uniform vec4 vertexColor;
-    void main()
-    {
-        color = vertexColor;
-    }
-)glsl";
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
-void check_compilation_status(GLuint shader) {
-    GLint success;
-    GLchar info_log[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(shader, 512, NULL, info_log);
-        std::cout << "Vertex shader compilation failed\n" << info_log << std::endl;
-    }
-}
-
-void check_link_status(GLuint program) {
-    GLint success;
-    GLchar info_log[512];
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(program, 512, NULL, info_log);
-        std::cout << "Shaders link failed\n" << info_log << std::endl;
-    }
-}
-
 int main() {
-//Инициализация GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -73,79 +35,47 @@ int main() {
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-
-    // create vertex shader
-    GLuint vertex_shader;
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
-    glCompileShader(vertex_shader);
-    check_compilation_status(vertex_shader);
-
-    // create fragment shader
-    GLuint fragment_shader;
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
-    glCompileShader(fragment_shader);
-    check_compilation_status(fragment_shader);
-
-    GLuint shader_program;
-    shader_program = glCreateProgram();
-
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    check_link_status(shader_program);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-
+    Shader shaders("../shaders/shader.vx",
+                   "../shaders/shader.frag");
     GLfloat vertices[] = {
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.0f
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
-    GLuint indices[] = {
-            0, 1, 3,
-            1, 2, 3
-    };
-
-    GLuint VBO, VAO, IBO;
+    GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &IBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
-
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), nullptr);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
     glBindVertexArray(0);
 
-
-
-    glUseProgram(shader_program);
+    shaders.Use();
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        shaders.Use();
 
         GLfloat time = glfwGetTime();
-        GLfloat green_value = (cos(time) / 2) + 0.5f;
-        GLuint color_location =  glGetUniformLocation(shader_program, "vertexColor");
-        glUniform4f(color_location, 0.0f, green_value, 0.0f, 1.0f);
+        GLfloat angle = (sin(time) / 2);
+        GLint angle_location = glGetUniformLocation(shaders.program, "angle");
+        glUniform3f(angle_location, angle, 0.0f, 0.0f);
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
@@ -153,7 +83,6 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &IBO);
     glfwTerminate();
     return 0;
 }
